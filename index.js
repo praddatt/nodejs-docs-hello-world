c33e1064693aaabb73320d73200763d2cdad5018
praddatt <50925263+praddatt@users.noreply.github.com>
2019-05-22 23:44:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World Now!");
+    response.end("Hello World Now 1!");
 });
 
 const port = process.env.PORT || 1337;
